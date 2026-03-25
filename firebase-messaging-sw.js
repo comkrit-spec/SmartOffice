af1781f7ea7120a7381a313fb8d2482f8e6acc5e
comkrit-spec <comkrit@asia-hydro.com>
2026-03-25 13:07:52 +0700

    Add files via upload

---
+++ firebase-messaging-sw.js
@@ -0,0 +1,23 @@
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-app-compat.js');
+importScripts('https://www.gstatic.com/firebasejs/10.8.1/firebase-messaging-compat.js');
+
+const firebaseConfig = {
+    apiKey: "AIzaSyA898_Q3mtKBETkUwY-v-wrIpDQ6LUafvg",
+    authDomain: "smartofficeapp-32e0a.firebaseapp.com",
+    projectId: "smartofficeapp-32e0a",
+    storageBucket: "smartofficeapp-32e0a.firebasestorage.app",
+    messagingSenderId: "412226486577",
+    appId: "1:412226486577:web:212028f120d1d940157010"
+};
+
+firebase.initializeApp(firebaseConfig);
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  const notificationTitle = payload.notification.title;
+  const notificationOptions = {
+    body: payload.notification.body,
+    icon: 'https://cdn-icons-png.flaticon.com/512/3214/3214011.png'
+  };
+  self.registration.showNotification(notificationTitle, notificationOptions);
+});
